a9da1e731c9c39cad7964dec93ee2ab473686c04
nika <nik.tukhashvili@makingscience.com>
2025-06-15 13:02:17 +0400

    add @Aspect annotation on LogAcpect class

--- src/main/java/org/example/java_4/aspect/LogAspect.java
+++ src/main/java/org/example/java_4/aspect/LogAspect.java
@@ -1,10 +1,12 @@
 package org.example.java_4.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Component
+@Aspect
 public class LogAspect {
 
     @Before("execution(* org.example.java_4.controller.*.*(..))")
